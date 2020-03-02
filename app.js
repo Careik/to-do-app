1613e45fb761bf62639546d5bbab882eddcd16b0
Eric Hellmuth <hellmuth07@gmail.com>
2020-03-02 18:34:24 -0500

    Updates - todo issue still

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 function onReady() {
+  let toDos = [];
+
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
+  function createNewTodo() = {
+    const newToDoText = document.getElementById('newToDoText');
 
-    //get the text
-    let title = newToDoText.value;
+    if(!newToDoText.value) { return;}
 
-    //create a new li
-    let newLi = document.createElement('li');
-
-
-    //create a new input
-    let checkbox = document.createElement('input');
-
-
-    //set the inputs type to checkbox
-    checkbox.type = "checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty thee input
+    toDos.push({
+      title: newToDoText.value,
+      complete: false;
+    });
     newToDoText.value = '';
 
+    renderTheUI();
+  }
 
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    })
+  }
+
+  }
+
+  addToDoForm.addEventListener('submit', event = {
+    event.preventDefault();
+    createNewTodo();
   });
+
+  renderTheUI();
+  }
 }
 
 window.onload = function() {
